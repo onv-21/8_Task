f658addc9137c069c9d7397ec3e6b4e08db9ccfd
Администратор <@DESKTOP-PJPICF5>
2021-11-24 22:12:38 +0800

    Добавьте файлы проекта.

---
+++ 8_2_Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+StreamWriter f1 = new StreamWriter("D:\\f1.txt");
+Random r = new Random();
+for (int i = 0; i < 10; i++) f1.WriteLine(r.Next(0, 100));
+f1.Close();
+StreamReader f = new StreamReader("D:\\f1.txt");
+string s = "";
+int[] array = new int [10];
+int Sum = 0;
+for (int i = 0; i < 10; i++)
+{
+    array[i] = Int32.Parse(f.ReadLine());
+    Sum += array[i];
+}
+f.Close();
+Console.WriteLine(Sum);
---
+++ 8_Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+string dirName = "C:\\";
+
+if (Directory.Exists(dirName))
+{
+    Console.WriteLine("Подкаталоги:");
+    string[] dirs = Directory.GetDirectories(dirName);
+    foreach (string s in dirs)
+    {
+        Console.WriteLine(s);
+    }
+    Console.WriteLine("Файлы:");
+    string[] files = Directory.GetFiles(dirName);
+    foreach (string s in files)
+    {
+        Console.WriteLine(s);
+    }
+}
